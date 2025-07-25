23b6db1dec717af662dc479405d4f93693b23103
kkkkkkkkkwon <ch80551@gmail.com>
2025-07-25 18:36:01 +0900

    feat: domain settings (#2)

---
+++ src/main/java/com/selfbell/device/domain/Device.java
@@ -0,0 +1,33 @@
+package com.selfbell.device.domain;
+
+import com.selfbell.user.domain.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "devices")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Device {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "user_id", unique = true)  // 일대일 대응 관계
+    private User user;
+
+    @Column(nullable = false)
+    private String deviceToken; //fcm_token
+
+    @Column(nullable = false)
+    private String deviceType; //platform
+
+    private LocalDateTime createdAt;
+}
