e4bba863abaff91a3ba13a48b5bfe78e4e2b5a06
kwakeunjae <emma_0407@naver.com>
2025-08-09 01:03:50 +0900

    "feat: jpa auditing 추가 및 엔티티 간 관계 오류 해결"

--- src/main/java/com/selfbell/SelfbellApplication.java
+++ src/main/java/com/selfbell/SelfbellApplication.java
@@ -2,7 +2,9 @@ package com.selfbell;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class SelfbellApplication {
     public static void main(String[] args) {
