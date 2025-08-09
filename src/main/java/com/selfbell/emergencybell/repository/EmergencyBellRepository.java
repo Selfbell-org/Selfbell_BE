9d252126f6c9d3d45a86536356ced3dc63859ef3
kkkkkkkkkwon <ch80551@gmail.com>
2025-08-10 02:26:41 +0900

    feat: emergency-bell 필터링으로 api에서 필요한 필드만 받기(dto 수정 후 버전)

--- src/main/java/com/selfbell/emergencybell/repository/EmergencyBellRepository.java
+++ src/main/java/com/selfbell/emergencybell/repository/EmergencyBellRepository.java
@@ -7,18 +7,23 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
 public interface EmergencyBellRepository extends JpaRepository<EmergencyBell, Long> {
 
-    // 반경 검색용: 필요한 컬럼만 SELECT (nativeQuery)
-    // 반환은 raw Object[] 리스트 -> Service에서 DTO로 매핑
-    @Query(value = "SELECT " +
-            "e.id, e.latitude, e.longitude, e.install_detail, e.management_phone, e.lot_number_address, e.install_type, e.coord_x, e.coord_y, " +
-            "(6371000 * acos(cos(radians(:userLat)) * cos(radians(e.latitude)) * cos(radians(e.longitude) - radians(:userLon)) + sin(radians(:userLat)) * sin(radians(e.latitude)))) AS distance " +
+    @Query(value = "SELECT * FROM (" +
+            "SELECT e.id, e.lat, e.lon, e.ins_DETAIL, e.mng_TEL, e.adres, e.ins_TYPE, e.x, e.y, " +
+            "(6371000 * acos(cos(radians(:userLat)) * cos(radians(e.lat)) * cos(radians(e.lon) - radians(:userLon)) + sin(radians(:userLat)) * sin(radians(e.lat)))) AS distance " +
             "FROM emergency_bell e " +
-            "HAVING distance <= :radiusInMeters " +
+            ") AS distances " +
+            "WHERE distance <= :radiusInMeters " +
             "ORDER BY distance", nativeQuery = true)
-    List<Object[]> findWithinRadiusRaw(
-            @Param("userLat") double userLat,
-            @Param("userLon") double userLon,
-            @Param("radiusInMeters") double radiusInMeters);
+    List<Object[]> findWithinRadiusRaw(@Param("userLat") double userLat,
+                                       @Param("userLon") double userLon,
+                                       @Param("radiusInMeters") double radiusInMeters);
 }
+
