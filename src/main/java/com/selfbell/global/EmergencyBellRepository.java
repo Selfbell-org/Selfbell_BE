562e0ed0ba85020ac8e696bbd38b7452bbee89c3
kkkkkkkkkwon <ch80551@gmail.com>
2025-08-17 18:46:06 +0900

    feat: 서버 작동 확인용

--- src/main/java/com/selfbell/global/EmergencyBellRepository.java
+++
@@ -1,7 +0,0 @@
-package com.selfbell.global;
-
-import com.selfbell.emergencybell.domain.EmergencyBell;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmergencyBellRepository extends JpaRepository<EmergencyBell, Long> {
-}
