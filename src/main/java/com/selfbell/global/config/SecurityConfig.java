587de3f3331ad038eb8a45cbd50063039e60310c
kwakeunjae <emma_0407@naver.com>
2025-08-18 21:04:44 +0900

    feat: 경로삭제

--- src/main/java/com/selfbell/global/config/SecurityConfig.java
+++ src/main/java/com/selfbell/global/config/SecurityConfig.java
@@ -30,11 +30,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
 
                 .authorizeHttpRequests(auth -> auth
-//                        .requestMatchers("/api/v1/**").permitAll()
                         .requestMatchers("/api/v1/auth/**").permitAll()
-                        .requestMatchers("/api/v1/criminals/**").permitAll()
-                        .requestMatchers("/api/v1/emergency-bells/**").permitAll()
-                        .requestMatchers("/api/v1/safe-walks/**").permitAll()  // 임시로 추가
                         .requestMatchers("/ws/**").permitAll()  // WebSocket 연결 허용
                         .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
