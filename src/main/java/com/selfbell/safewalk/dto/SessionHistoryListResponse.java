3134cb286bf4882bc434c215d43f1e32d9f028b2
shinheekim <dev.shinheekim@gmail.com>
2025-08-23 18:20:38 +0900

    chore: 불필요한 @Builder 삭제 (#19)

--- src/main/java/com/selfbell/safewalk/dto/SessionHistoryListResponse.java
+++ src/main/java/com/selfbell/safewalk/dto/SessionHistoryListResponse.java
@@ -1,10 +1,7 @@
 package com.selfbell.safewalk.dto;
 
-import lombok.Builder;
-
 import java.util.List;
 
-@Builder
 public record SessionHistoryListResponse(
         List<SessionHistoryItem> sessions
 ) {
